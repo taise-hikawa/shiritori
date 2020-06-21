a5f10a6f30ca5d9b123d9600075d1a9bdb9129fe
taisei.hikawa <cmmkarubi@gmail.com>
2020-06-22 00:30:26 +0900

    Initial Commit

---
+++ shiritori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shiritori
+//
+//  Created by Sakurako Shimbori on 2020/06/22.
+//  Copyright © 2020 Taisei Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
