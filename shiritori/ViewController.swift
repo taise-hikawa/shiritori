feacee06ef05ffe26f076117e691b2cf0946c7cb
taisei.hikawa <cmmkarubi@gmail.com>
2020-06-22 21:59:23 +0900

    20200622

--- shiritori/ViewController.swift
+++ shiritori/ViewController.swift
@@ -10,20 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    // 文字列保存用の変数
+    var textFieldString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        textLabel.text=""
-    }
-    @IBAction func reset(_ sender: Any) {
-        textLabel.text=""
+        textLabel.text = "しりとり"
     }
     
-    @IBAction func testSend(_ sender:UISlider) {
-        textLabel.text="\(sender.value)"
-        
+    @IBAction func reset(_ sender: UIButton) {
+        textLabel.text = "しりとり"
     }
+    @IBAction func textSend(_ sender: UIButton) {
+        // TextFieldから文字を取得
+        textFieldString = textField.text!
+
+        // TextFieldの中身をクリア
+        textField.text = ""
+    }
+    
     
 }
 
