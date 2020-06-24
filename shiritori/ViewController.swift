99ef89e6961392ef6473ff9340d98024d604a987
taisei.hikawa <cmmkarubi@gmail.com>
2020-06-25 00:30:01 +0900

    shiritori20200625

--- shiritori/ViewController.swift
+++ shiritori/ViewController.swift
@@ -8,27 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITextFieldDelegate{
+    @IBOutlet weak var myTextField: UITextField!
     @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var textField: UITextField!
     // 文字列保存用の変数
     var textFieldString = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        textLabel.text = "しりとり"
+        myTextField.delegate = self
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        //入力された文字を取り出す
+        if let word = myTextField.text{
+            if word.prefix(1) == textLabel.text?.suffix(1){
+                textLabel.numberOfLines += 1
+                textLabel.text = textLabel.text! + "\n" + word
+        
+            }else{
+                textLabel.numberOfLines += 1
+                textLabel.text = textLabel.text! + "\n" + "残念Resetを押してね"
+            }
+        }
+        return true
     }
     
     @IBAction func reset(_ sender: UIButton) {
         textLabel.text = "しりとり"
-    }
-    @IBAction func textSend(_ sender: UIButton) {
-        // TextFieldから文字を取得
-        textFieldString = textField.text!
-
-        // TextFieldの中身をクリア
-        textField.text = ""
+        textLabel.numberOfLines = 1
     }
     
     
